ab2f2e900a018bc0c72042d8760802a14e65f946
depler <depler.mv@gmail.com>
2024-02-11 19:14:11 +0300

    curl-impersonate

--- build.bat
+++ build.bat
@@ -10,25 +10,8 @@ ninja.exe -C lib crypto ssl
 move /y lib\crypto\libcrypto.a lib\libcrypto.a
 move /y lib\ssl\libssl.a lib\libssl.a
 
-set IPV6=1
+set CFG=-zlib -zstd -brotli -nghttp2 -idn2 -ssl
 
-set ZLIB=1
-set ZLIB_PATH=zlib_stub
-
-set ZSTD=1
-set ZSTD_PATH=zstd_stub
-
-set BROTLI=1
-set BROTLI_PATH=brotli_stub
-set BROTLI_LIBS=-lbrotlidec -lbrotlicommon
-
-set NGHTTP2=1
-set NGHTTP2_PATH=nghttp2_stub
-
-set IDN2=1
-set LIBIDN2_PATH=idn2_stub
-
-set SSL=1
 set OPENSSL_PATH=%ROOT%boringssl
 set OPENSSL_LIBPATH=%ROOT%boringssl\lib
 set OPENSSL_LIBS=-lssl -lcrypto
--- patch_curl.bat
+++ patch_curl.bat
@@ -4,13 +4,9 @@ set sed=c:\msys64\usr\bin\sed.exe
 cd %~dp0curl
 
 %patch% -p1 < %~dp0patch\curl-impersonate.patch
-%sed% -i 's/-shared/-s -static -shared/g' lib\Makefile.m32
-%sed% -i 's/-static/-s -static/g' src\Makefile.m32
 
-%sed% -i 's/-DUSE_NGHTTP2/-DUSE_NGHTTP2 -DNGHTTP2_STATICLIB/g' lib\Makefile.m32
-%sed% -i 's/-DUSE_NGHTTP2/-DUSE_NGHTTP2 -DNGHTTP2_STATICLIB/g' src\Makefile.m32
-
-%sed% -i 's/-lidn2/-lidn2 -lunistring -liconv/g' lib\Makefile.m32
-%sed% -i 's/-lidn2/-lidn2 -lunistring -liconv/g' src\Makefile.m32
+%sed% -i 's/-shared/-s -static -shared/g' lib\Makefile.mk
+%sed% -i 's/-DUSE_NGHTTP2/-DUSE_NGHTTP2 -DNGHTTP2_STATICLIB/g' lib\Makefile.mk
+%sed% -i 's/-lidn2/-lidn2 -lunistring -liconv/g' lib\Makefile.mk
 
 cd ..
